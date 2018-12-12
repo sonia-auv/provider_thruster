e27697ade3de6cefa978cca143a4e6226cca11d2
alec <alec.st-marseille@hotmail.com>
2018-12-12 14:59:48 -0500

    modif provider to fit with proc_control

--- src/provider_thruster/provider_thruster_node.cc
+++ src/provider_thruster/provider_thruster_node.cc
@@ -4,6 +4,8 @@
 
 #include "provider_thruster/provider_thruster_node.h"
 #include <string>
+#include <iostream>
+#include <fstream>
 
 namespace provider_thruster {
 
@@ -12,8 +14,26 @@ namespace provider_thruster {
 
     //------------------------------------------------------------------------------
     //
-    ProviderThrusterNode::ProviderThrusterNode(ros::NodeHandlePtr &nh) : nh_(nh)
+    ProviderThrusterNode::ProviderThrusterNode(ros::NodeHandlePtr &nh) :
+        nh_(nh),
+        fichier{"T1","T2","T3","T4","T5","T6","T7","T8"},
+        calcul(8,6)
     {
+
+        YAML::Node node = YAML::LoadFile(file_path_);
+
+        int j=0;
+        for(auto&t : fichier){
+            auto thruster = node[t];
+            assert(thruster.Type() == YAML::NodeType::Sequence);
+           // std::array<double, 6> force_array = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+            for (uint8_t i = 0; i < 6; i++) {
+                calcul(j,i) = thruster[i].as<double>();
+            }
+
+            j++;
+        }
+
 
         thrusterEffortSubscriber = nh->subscribe("/provider_thruster/thruster_effort", 1000, &ProviderThrusterNode::thrusterEffortCallback, this);
 
@@ -56,28 +76,37 @@ namespace provider_thruster {
 
     //------------------------------------------------------------------------------
     //
-    void ProviderThrusterNode::thrusterEffortCallback(const ThrusterEffort::ConstPtr& msg)
+    void ProviderThrusterNode::thrusterEffortCallback(const geometry_msgs::Wrench & msg)
     {
-        ROS_DEBUG("Message received : {ID: %u, effort: %i}", msg->ID, msg->effort);
 
         rs485Msg.cmd = interface_rs485::SendRS485Msg::CMD_ISI_power;
 
-        int effort = msg->effort;
+        vecteur[0]=msg.force.x;
+        vecteur[1]=msg.force.y;
+        vecteur[2]=msg.force.z;
+        vecteur[3]=msg.torque.x;
+        vecteur[4]=msg.torque.y;
+        vecteur[5]=msg.torque.z;
 
-        if (effort < -100) {
-          power[msg->ID - 1] = 0;
-        }
-        else if (effort > 100) {
-          power[msg->ID - 1] = 200;
-        }
-        else {
-          power[msg->ID - 1] = effort + 100;
+        motors_in = calcul*vecteur;
+
+        rs485Msg.data.clear();
+        for(uint8_t j=0;j<8;j++) {
+
+            if (motors_in [j] <-100) {
+                motors_out[j] = 0;
+
+            } else if (motors_in[j] > 100) {
+                motors_out[j] = 200;
+
+            } else {
+                motors_out[j] = motors_in[j] + 100;
+            }
+            rs485Msg.data.push_back(motors_out(j,1));
         }
 
       rs485Msg.data.clear();
-      for(uint8_t i = 0; i < 8; i++) {
-        rs485Msg.data.push_back(power[i]);
-      }
+
 
       rs485Msg.slave = interface_rs485::SendRS485Msg::SLAVE_ISI_PWM;
 
